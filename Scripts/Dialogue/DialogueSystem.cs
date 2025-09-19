7dfc0a86fbd2ac6323c28fcda19de2fc476a922d
Tórbjørn Rygaard <mtr0607@hotmail.dk>
2025-09-19 19:41:14 +0200

    Portraits implemented

--- Scripts/Dialogue/DialogueSystem.cs
+++ Scripts/Dialogue/DialogueSystem.cs
@@ -5,8 +5,8 @@ public partial class DialogueSystem : CanvasLayer
 {
 
     [Export(PropertyHint.File)] public String DialogueFile;
-    [Export] private RichTextLabel _nameLabel;
-    [Export] private RichTextLabel _messageLabel;
+    [Export] private RichTextLabel _nameLabel, _messageLabel;
+    [Export] private TextureRect _portrait1, _portrait2;
     private DialogueEntry[] Dialogues;
     private int currentIndex = 0;
 
@@ -52,7 +52,36 @@ public partial class DialogueSystem : CanvasLayer
     {
         var entry = Dialogues[currentIndex];
         _nameLabel.Text = entry.name;
+
         _messageLabel.Text = entry.message;
+
+        ChangePortraits();
+    }
+
+    private void ChangePortraits()
+    {
+        var entry = Dialogues[currentIndex];
+        try
+        {
+            Texture2D portraitTexture = (Texture2D)GD.Load("res://Sprites/Dialogue Portraits/" + entry.portrait1 + ".png");
+            _portrait1.Texture = portraitTexture;
+            GD.Print("Changed portrait1 to: " + entry.portrait1);
+        }
+        catch (Exception e)
+        {
+            GD.Print("No portrait1 declared in JSON file. ERROR: " + e);
+        }
+
+        try
+        {
+            Texture2D portraitTexture = (Texture2D)GD.Load("res://Sprites/Dialogue Portraits/" + entry.portrait2 + ".png");
+            _portrait2.Texture = portraitTexture;
+            GD.Print("Changed portrait2 to: " + entry.portrait2);
+        }
+        catch (Exception e)
+        {
+            GD.Print("No portrait2 declared in JSON file. ERROR: " + e);
+        }
     }
 
 }
@@ -61,4 +90,6 @@ public class DialogueEntry
 {
     public string name { get; set; }
     public string message { get; set; }
+    public string portrait1 { get; set; }
+    public string portrait2 { get; set; }
 }
