a4bcae5cf622adbb8cca495a07609e53c9e3d3f6
Tórbjørn Rygaard <mtr0607@hotmail.dk>
2025-09-28 16:15:30 +0200

    Added vision cone (Can still see through walls)

--- Scripts/Creatures & AI/MissileToad.cs
+++ Scripts/Creatures & AI/MissileToad.cs
@@ -9,11 +9,12 @@ public partial class MissileToad : Creature
     [Export] private Node2D _fireLocation;
     private Timer _timer;
 
+    private bool _isAlert = false;
+
 
     public override void _Ready()
     {
         _timer = GetNode<Timer>("Timer");
-        _timer.Start();
     }
 
     public override void _PhysicsProcess(double delta)
@@ -22,6 +23,22 @@ public partial class MissileToad : Creature
         MoveAndSlide();
     }
 
+    private void OnVisionEntered(Area2D PlayerArea)
+    {
+        PhysicsDirectSpaceState2D DirectState = GetWorld2D().DirectSpaceState;
+        var RayQuery = PhysicsRayQueryParameters2D.Create(this.Position, PlayerArea.Position);
+        var RayCollidedObject = DirectState.IntersectRay(RayQuery);
+
+        //GD.Print((GodotObject)RayCollidedObject[0]);
+        GD.Print(RayCollidedObject.Keys);
+
+        _isAlert = true;
+        GD.Print(this + " was alerted!");
+        // Add in an some visual feedback for when the creature becomes alert.
+
+        // Starting the timer makes the enemy start firing missiles.
+        _timer.Start();
+    }
 
     public void OnTimerTimeout()
     {
