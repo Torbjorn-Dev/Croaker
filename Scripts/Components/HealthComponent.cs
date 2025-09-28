717bf298820838f7db7a0c0a1a9c3e56e459e35a
Tórbjørn Rygaard <mtr0607@hotmail.dk>
2025-09-28 15:44:50 +0200

    Implemented I-Frames (Graphics missing)

--- Scripts/Components/HealthComponent.cs
+++ Scripts/Components/HealthComponent.cs
@@ -10,21 +10,37 @@ public partial class HealthComponent : Node
     [Export] private int _maxHealth;
     private int _currentHealth;
 
+    private bool _isInvincible = false;
+
     public override void _Ready()
     {
         _currentHealth = _maxHealth;
     }
 
-
     public void TakeDamage(int DamageTaken)
     {
-        GD.Print("Took " + DamageTaken + " damage!");
-        _currentHealth -= DamageTaken;
-        if (_currentHealth <= 0)
+        if (!_isInvincible)
         {
-            EmitSignal(SignalName.NoHealth);
+            GD.Print("Took " + DamageTaken + " damage!");
+            _currentHealth -= DamageTaken;
+
+            if (_currentHealth <= 0)
+            {
+                EmitSignal(SignalName.NoHealth);
+            }
+            StartIFrames();
         }
     }
 
+    private void StartIFrames()
+    {
+        _isInvincible = true;
+        GetChild<Timer>(0).Start();
+    }
+
+    private void OnInvincibilityTimerTimeout()
+    {
+        _isInvincible = false;
+    }
 
 }
