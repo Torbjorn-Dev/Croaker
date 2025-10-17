b35a25508d88ebbe9c54d680f5af0f4bb4ea18b7
Tórbjørn Rygaard <mtr0607@hotmail.dk>
2025-10-17 19:08:26 +0200

    Switched back to 2D camera

--- Scripts/GameCamera3D.cs
+++ Scripts/GameCamera3D.cs
@@ -4,10 +4,12 @@ using System;
 public partial class GameCamera3D : Camera3D
 {
     private Player _player;
+    private Camera2D _mouseTracker;
 
     public override void _Ready()
     {
         _player = (Player)GetTree().GetNodesInGroup("Player")[0];
+        _mouseTracker = GetNode<Camera2D>("MouseTracker");
     }
 
     public override void _PhysicsProcess(double delta)
@@ -24,12 +26,15 @@ public partial class GameCamera3D : Camera3D
 
     public void FollowMouse()
     {
-        Position = Position.Lerp(_player.Position.Lerp(GetGlobalMousePosition(), 0.35f), 0.05f);
+        GD.Print("Camera following mouse!");
+        Vector3 mousePosition = new Vector3(_mouseTracker.GetGlobalMousePosition().X, _mouseTracker.GetGlobalMousePosition().Y, 0);
+        Position = Position.Lerp(new Vector3(_player.Position.X, _player.Position.Y, 0).Lerp(mousePosition, 0.35f), 0.05f);
+        GD.Print("Mouse position: " + mousePosition);
     }
 
     public void FollowPlayer()
     {
-        float DistanceMultiplier = Math.Min(25, Position.DistanceTo(_player.Position) * 0.01f);
-        Position = Position.Lerp(_player.Position, 0.01f * DistanceMultiplier);
+        float DistanceMultiplier = Math.Min(25, Position.DistanceTo(new Vector3(_player.Position.X, _player.Position.Y, 0) * 0.01f));
+        Position = Position.Lerp(new Vector3(_player.Position.X, _player.Position.Y, 0), 0.01f * DistanceMultiplier);
     }
 }
